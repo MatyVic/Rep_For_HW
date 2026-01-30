0fe8d98a1f34bc178a7482c9ff459579c52d94b0
MatyVic <thexcvb@gmail.com>
2026-01-30 02:57:43 +0200

    Виконання ДЗ 4.1 4.2 4.3

--- 04/HomeWork04ex3.py
+++ 04/HomeWork04ex3.py
@@ -1,11 +1,11 @@
 # ДЗ 4.3. Список із 3 елементів
-
 import random as rnd
 
 my_lst = []
-
+# Наповнюємо список випадкової довжини випадковими елементами
 for i in range(rnd.randint(3, 10)):
-   my_lst.append(rnd.randint(0, 25))
+    my_lst.append(rnd.randint(0, 25))
 
 print("Створений список:", my_lst)
-print("Результуючий список з трьох елементів", [my_lst[0], my_lst[2], my_lst[-2]])
+print("Результуючий список з трьох елементів",
+      [my_lst[0], my_lst[2], my_lst[-2]])
