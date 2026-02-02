9aa1166d1527d5f03efe75cd041b9c16e1eee5bf
MatyVic <thexcvb@gmail.com>
2026-02-02 19:52:15 +0200

    Виконання ДЗ 5.1 5.2 5.3

--- 05/HomeWork05ex3.py
+++ 05/HomeWork05ex3.py
@@ -1,13 +1,22 @@
 # 5.3. hashtag
 import string
 
-str_to_hashtag = input("Введіть фразу для перетворення ")
-new_str = ""
+while True:
+    str_to_hashtag = input("Введіть фразу для перетворення ")
+    new_str = ""
 
-for ch in str_to_hashtag:
-    if ch in string.punctuation:
-        ch = ""
+    for ch in str_to_hashtag:
+        if ch in string.punctuation:
+            ch = ""
+        else:
+            new_str += ch
+
+    print("#" + new_str[:140].title().replace(" ", ""))
+
+    call_check = input("Хочете продовжити роботу?"
+                       " (введіть y/yes) ").lower().strip()
+    if call_check in ("y", "yes"):
+        continue
     else:
-        new_str += ch
+        break
 
-print("#" + new_str[:140].title().replace(" ", ""))
