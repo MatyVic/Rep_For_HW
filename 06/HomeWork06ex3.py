ed70fc56425f91d4d5f87a84b4f0ddde2949558f
MatyVic <thexcvb@gmail.com>
2026-02-04 13:33:35 +0200

    Виконання ДЗ 6.1 6.2 6.3

---
+++ 06/HomeWork06ex3.py
@@ -0,0 +1,15 @@
+# ДЗ 6.3. Добуток чисел
+
+exit_check = "y"
+while exit_check in ["y", "yes", "т", "так"]:
+
+    my_num = int(input("Введіть число: "))
+
+    while my_num > 9:
+        res = 1
+        for item in str(my_num):
+            res *= int(item)
+        my_num = res
+
+    print(my_num)
+    exit_check = input("Продовжити? (y/ yes/т/так) ").lower()
