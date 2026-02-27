9db08ac71eda34c57224c19ee965a1865ef54c1f
VicMaty <thexcvb@gmail.com>
2026-02-27 19:38:59 +0200

    Оновлення доп робіт #2

--- 12/HomeWork12ex1.py
+++ 12/HomeWork12ex1.py
@@ -1,4 +1,17 @@
-import math
+# ДЗ 12.1. Очистити текст від html-тегів
+import re
 
-print(math.factorial(5))
-print(math.factorial(10))
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+      with open(html_file, 'r') as file:
+          html = file.read()
+          text = ''
+          for line in html.splitlines():
+               start = line.find(">") + 1
+               end = line.find("</")
+               if start > 0 and end > start:
+                    text += line[start:end].strip() + '\n'
+
+          result_file = open(result_file, 'w').write(text)
+
+delete_html_tags('html_file.html')
